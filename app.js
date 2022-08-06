f20b297231094b1462f8782eae3de0bd54ee3317
Rohit Patil <rohitvpatil0810@gmail.com>
2022-08-07 00:51:19 +0530

    Deploying

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const Router = require("./routers");
-const cors = require("cors");
 dotenv.config({ path: "./config.env" });
 const app = express();
 
 const dbURI = process.env.DATABASE;
 const port = process.env.PORT || 5000;
 
-let corsOptions = {
-  origin: ["http://localhost:5500", "http://localhost:3000"],
-};
-
-app.use(cors(corsOptions));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(Router);
@@ -25,5 +20,11 @@ mongoose
     console.log("connected to mongodb and listening at port 5000");
   })
   .catch((err) => console.error(err));
-// app.use(authRouter);
-// app.use(expenseRouter);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
